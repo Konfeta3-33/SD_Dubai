1c0d664dd8a9ccf47d49187a034d57cff08ff9ec
Aleksandr Kunin <skyksandr@gmail.com>
2022-05-19 21:04:44 +0400

    Fix rest of rubocop errors

--- db/migrate/20220419152029_add_login_to_users.rb
+++ db/migrate/20220419152029_add_login_to_users.rb
@@ -1,5 +1,5 @@
 class AddLoginToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :login, :string, null: false, index: { unique: true } # rubocop:disable Rails/NotNullColumn
+    add_column :users, :login, :string, null: false # rubocop:disable Rails/NotNullColumn
   end
 end
