1aa87f0cd68a9eee2fb11efec16e297df6f29196
Valentina Kunina <konfeta333@gmail.com>
2022-05-20 14:14:53 +0400

    Fix rubocop errors after merge

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   belongs_to :profile, polymorphic: true
 
-  # TODO: enable Rails/UniqueValidationWithoutIndex
-  validates :login, uniqueness: true # rubocop:disable Rails/UniqueValidationWithoutIndex
+  validates :login, uniqueness: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_19_133722) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_20_093101) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_19_133722) do
     t.boolean "admin", default: false, null: false
     t.string "profile_type"
     t.bigint "profile_id"
+    t.index ["login"], name: "index_users_on_login", unique: true
     t.index ["profile_type", "profile_id"], name: "index_users_on_profile"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
