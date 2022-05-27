2a98a62b8db042ce5b87ffa16145cb18497f9e30
Valentina Kunina <konfeta333@gmail.com>
2022-05-27 17:55:46 +0400

    Fixed module/class Credentials

--- app/controllers/instructors/credentials_controller.rb
+++ app/controllers/instructors/credentials_controller.rb
@@ -1,26 +1,28 @@
-class Instructors::CredentialsController < ApplicationController
-  before_action :set_instructor
+module Instructors
+  class CredentialsController < ApplicationController
+    before_action :set_instructor
 
-  def edit
-  end
-
-  def update
-    if @instructor.update instructor_params
-      flash[:notice] = "Instructor's credentials #{@instructor.name} #{@instructor.surname}
-updated!"
-      redirect_to @instructor
-    else
-      render :edit, status: :unprocessable_entity
+    def edit
     end
-  end
 
-  private
+    def update
+      if @instructor.update instructor_params
+        flash[:notice] = "Instructor's credentials #{@instructor.name} #{@instructor.surname}
+updated!"
+        redirect_to @instructor
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
 
-  def set_instructor
-    @instructor = Instructor.find(params[:instructor_id])
-  end
+    private
 
-  def instructor_params
-    params.require(:instructor).permit(user_attributes: [:login, :password])
+    def set_instructor
+      @instructor = Instructor.find(params[:instructor_id])
+    end
+
+    def instructor_params
+      params.require(:instructor).permit(user_attributes: [:login, :password])
+    end
   end
 end
