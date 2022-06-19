82e4259fd2b9486e957866eadfbdfec1852fb0ec
Aleksandr Kunin <skyksandr@gmail.com>
2022-06-19 17:29:56 +0700

    Improve page styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,16 +9,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, authentication_keys: [:login]
 
-  def email_required?
-    false
-  end
+  def email_required? = false
 
-  def email_changed?
-    false
-  end
+  def email_changed? = false
 
-  # use this instead of email_changed? for Rails = 5.1.x
-  def will_save_change_to_email?
-    false
-  end
+  def will_save_change_to_email? = false
 end
