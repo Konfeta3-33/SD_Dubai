6f41e8c38b335b3c18b33493586a935be0e9b374
Aleksandr Kunin <skyksandr@gmail.com>
2022-06-23 10:29:56 +0700

    Setup tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,8 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  headless = ENV['SELENIUM_HEADLESS_CHROME'].present?
+  browser = headless ? :headless_chrome : :chrome
+
+  driven_by :selenium, using: browser, screen_size: [1400, 1400]
 end
